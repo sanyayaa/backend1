24d50ef6f70fb6e5b55cf0cd0ab577a9536cf03e
Sanya <sanyayaatiwari@gmail.com>
2023-06-06 11:26:03 +0530

    CRUD

--- index.js
+++ index.js
@@ -9,89 +9,71 @@ const products = data.products;
 //server start
 const server = express();
 
-
-
+//bodyParser
 server.use(express.json());
-
-server.use(morgan('dev'))
-
-
-//static hosting - slash / k baad tum ese directly access kr sakte ho middleware ki zarurat nhi hogi 
-//built in static middleware
+server.use(morgan('default'))
 server.use(express.static('public')); 
 
 
-// MIDDLEWARE 
-// example - jaise ap kisi country se req bheji kisi website mein if vo website nhi de allow karti apki country ko toh vo vapis bhej deti hai ya allow kr deti hai .
+//API
+//Products
+//API ROOT,base URL, example - google.com/api/v2
+// C R U D
 
-// server.use((req,res,next) =>{
-//   //yeh waha req.f=get dusre wala get hai info laa kr deta hai 
-//   console.log(req.method,req.ip,req.hostname,new Date(),req.get('User-Agent'))
-//   next()
-// })
-
-
-const auth = ((req,res,next) =>{
-  // console.log(req.query)
-  // if(req.query.password == '123'){
-  //   next()
-  // }
-
-//express req  ki body ko nhi nikalta apne aap.so we use built in middleware for this we'll use express.json middleware
-  if(req.body.password == '123'){
-    next()
-  }else{
-    res.sendStatus(401);
-  }
-  next()
-})
-
-// yeh pure program mein auth wala middleware laga raha hai jo ki galat hai kyuki sab mein auth thodi lagega so hum route mein hi auth wlaa middleware daal denge jaha chaiye jaise login sign up mein 
-
-// server.use(auth);
-
-
-// check kro auth chll raha hai ya nhi yeh daalo url 
-// http://localhost:8080/?password=123
-
-//API - Endpoint - Route
-// server.get('/',auth,(req,res) => {
-//   res.json({type:'GET'})
-// })
-server.get('/product/:id',auth,(req,res) => {
-  // yeh jo /:id kia hai ese bolte hai url parameter mtlab product k baad kuch bhi ho sakta hai product static hai 
-  console.log(req.params)
-  res.json({type:'GET'})
-})
-server.post('/',auth,(req,res) => {
-  res.json({type:'POST'})
-})
-server.put('/',(req,res) => {
-  res.json({type:'PUT'})
-})
-server.delete('/',(req,res) => {
-  res.json({type:'DELETE'})
-})
-server.patch('/',(req,res) => {
-  res.json({type:'PATCH'})
+//CREATE API usnig POST
+//Create POST/products
+server.post('/products',(req,res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.status(201).json(req.body)
 })
 
 
 
+//READ API   GET/products
+server.get('/products',(req,res) => {
+  res.json(products);
+})
+
+//READ API   GET/products/:id
+server.get('/products/:id',(req,res) => {
+  //vaise yeh string id thi toh convert krne k liyen umber mein we added +
+  const id = +req.params.id;
+  const product = products.find(p=>p.id===id)
+  res.json(product);
+})
+
+
+//UPDATE PUT /products/:id
+// put mein last data ko overwrite kr dia jata hai 
+server.put('/products/:id',(req,res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  products.splice(productIndex,1,{...req.body,id})
+  res.status(201).json();
+})
+//patch mein overwrite hi krte jo mein change kr raha hu vo change ho baaki ka sab same rahe
+
+//UPDATE PATCH /products/:id
+server.patch('/products/:id',(req,res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  const product = products[productIndex];
+  products.splice(productIndex,1,{...product,...req.body})
+  res.status(201).json();
+})
 
 
 
+//DELETE DELETE /products/:id
+server.delete('/products/:id',(req,res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  const product = products[productIndex];
+  products.splice(productIndex,1)
+  res.status(201).json(product);
+})
 
-
-
-
-// server.get('/demo',(req,res) => {
-  // res.send('<h1>hello world</h1>');
-  // res.sendFile('/Users/Sanya/Desktop/BACKEND/New folder/index.html');
-  // res.json(products);
-  // res.sendStatus(404);
-  // res.status(201).send('<h1>hello</h1>');
-// })
 
 
 //server end
